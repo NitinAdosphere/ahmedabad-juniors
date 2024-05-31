98588607fa07d2db1334833be6d71c620c6b93a1
Nitin Bharsadiya <nitin@adosphereindia>
2024-05-31 16:24:21 +0530

    init site

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import "./home.css";
+export default function Home() {
+  return (
+    <>
+      <section className="banner-carousel-section">
+        <div>Home Pagedev</div>
+        <h2>Text</h2>
+      </section>
+    </>
+  );
+}
