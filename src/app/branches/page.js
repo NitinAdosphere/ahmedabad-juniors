752425596ef0e5aa4a43aee0d3447071d466e041
Masum <trivedimasum756@gmail.com>
2024-06-18 14:19:31 +0530

    branches page completed

---
+++ src/app/branches/page.js
@@ -0,0 +1,190 @@
+import React from "react";
+import "./branches.css";
+
+const page = () => {
+  return (
+    <div>
+      <section id="branch-conetnt content" class="clearfix purple-bg">
+        <div class="content-wrap">
+          <div class="section-title center">
+            <h1 class="white">Our Branches</h1>
+          </div>
+          <div class="our-branches-page-section section notoppadding purple-bg nomargin">
+            <div class="container">
+              <div class="our-branches-detail">
+                <div class="row clearfix common-height">
+                  <div class="col-md-12 col-lg-6 col-our-branches">
+                    <div class="our-branches-item">
+                      <h3 class="inner-page-section-title">Maninagar</h3>
+                      <p>
+                        1, Bhakt Prahlad co operative society, Opp. Jay Somnath
+                        school, Behind Methodist Church, Near Paras provision
+                        store, Maninagar (East)- 380008
+                      </p>
+                      <p>
+                        Contact No :{" "}
+                        <a href="tel:+91 9879611921"> 98796 11921</a>{" "}
+                      </p>
+                      <p>
+                        Email Id:{" "}
+                        <a href="mailto:maninagareast@ahmedabadjuniors.com">
+                          maninagareast@ahmedabadjuniors.com
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                  <div class="col-md-12 col-lg-6 col-our-branches">
+                    <div class="our-branches-item">
+                      <h3 class="inner-page-section-title">New Gota</h3>
+                      <p>
+                        Simandhar Status, Vodafone Tower Lane, Near Shayona
+                        Green, Gota, Ahmedabad - 382481
+                      </p>
+                      <p>
+                        Contact No :{" "}
+                        <a href="tel:+91 9512590900"> 95125 90900</a>{" "}
+                      </p>
+                      <p>
+                        Email Id:{" "}
+                        <a href="mailto:newgota@ahmedabadjuniors.com">
+                          newgota@ahmedabadjuniors.com
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                  <div class="col-md-12 col-lg-6 col-our-branches">
+                    <div class="our-branches-item">
+                      <h3 class="inner-page-section-title">New Ranip</h3>
+                      <p>
+                        17-18 Ganesh Homes, Nr. Sahajand Homes, Chenpur Road ,
+                        New Ranip, Ahemedabad - 382470
+                      </p>
+                      <p>
+                        Contact No :{" "}
+                        <a href="tel:+91 9428770180"> 94287 70180</a>{" "}
+                      </p>
+                      <p>
+                        Email Id:{" "}
+                        <a href="mailto:newranip@ahmedabadjuniors.com">
+                          newranip@ahmedabadjuniors.com
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+
+                  <div class="col-md-12 col-lg-6 col-our-branches">
+                    <div class="our-branches-item">
+                      <h3 class="inner-page-section-title">New Chandkheda</h3>
+                      <p>
+                        Satya Square, FF 9,10,11 Tragad Road New Chandkheda,
+                        Ahmedabad - 382424
+                      </p>
+                      <p>
+                        Contact No :{" "}
+                        <a href="tel:+91 9638836868"> 96388 36868</a>{" "}
+                      </p>
+                      <p>
+                        Email Id:{" "}
+                        <a href="mailto:newchandkheda@ahmedabadjuniors.com">
+                          newchandkheda@ahmedabadjuniors.com
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+
+                  <div class="col-md-12 col-lg-6 col-our-branches">
+                    <div class="our-branches-item">
+                      <h3 class="inner-page-section-title">New CG road</h3>
+                      <p>
+                        26, Sahjanand villa 1, New CG road Chandkheda, Ahmedabad
+                        Gujarat - 382424
+                      </p>
+                      <p>
+                        Contact No :{" "}
+                        <a href="tel:+917567106109"> 75671 06109</a>{" "}
+                      </p>
+                      <p>
+                        Email Id:{" "}
+                        <a href="mailto:newcgroad@ahmedabadjuniors.com">
+                          newcgroad@ahmedabadjuniors.com
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+
+                  <div class="col-md-12 col-lg-6 col-our-branches">
+                    <div class="our-branches-item">
+                      <h3 class="inner-page-section-title">New Shahibaug</h3>
+                      <p>
+                        112, shyam elegance, New Shahibaug, Nana Chiloda,
+                        Ahmedabad, Gujarat 382330
+                      </p>
+                      <p>
+                        Contact No :{" "}
+                        <a href="tel:+917575000221"> 75750 00221</a>{" "}
+                      </p>
+                      <p>
+                        Email Id:{" "}
+                        <a href="mailto:ajnewshahibaug@ahmedabadjuniors.com">
+                          ajnewshahibaug@ahmedabadjuniors.com
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+
+                  <div class="col-md-12 col-lg-6 col-our-branches">
+                    <div class="our-branches-item">
+                      <h3 class="inner-page-section-title">Tragad</h3>
+                      <p>
+                        IJK 101&102, Anand iLYF, Opp. Diwali Elegance, Tragad
+                        Ahmedabad - 382470
+                      </p>
+                      <p>
+                        Contact No : <a href="tel:+919979979656"> 9979979656</a>{" "}
+                      </p>
+                      <p>
+                        Email Id:{" "}
+                        <a href="mailto:ajtragad@ahmedabadjuniors.com">
+                          ajtragad@ahmedabadjuniors.com
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                  <div class="col-md-12 col-lg-6 col-our-branches">
+                    <div class="our-branches-item">
+                      <h3 class="inner-page-section-title">Ghatlodiya</h3>
+                      <p>
+                        57, Harshidh Baug society, Ghatlodiya Ahmedabad - 380061
+                      </p>
+                      <p>
+                        Contact No : <a href="tel:+919824322763"> 9824322763</a>{" "}
+                      </p>
+                      <p>
+                        Contact No : <a href="tel:+919924855127"> 9924855127</a>{" "}
+                      </p>
+                      <p>
+                        Email Id:{" "}
+                        <a href="mailto:ajghatlodiya@ahmedabadjuniors.com">
+                          ajghatlodiya@ahmedabadjuniors.com
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <a class="home-link" href="<?php echo $siteurl; ?>">
+              {" "}
+              <img
+                src="<?php echo $siteurl; ?>images/home-blue.webp"
+                alt="Home Link"
+              />
+            </a>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default page;
